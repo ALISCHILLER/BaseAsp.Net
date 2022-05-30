a96ec7c715e2326761d5280d82dcf55caf50cb5c
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-05-30 14:37:13 +0430

    Ascinc Multitrading

--- Session1/Ascinc_Multitrading/Program.cs
+++ Session1/Ascinc_Multitrading/Program.cs
@@ -1,4 +1,31 @@
+//? File Scope Namespace
+
+//? Explicit Using
+// using Microsoft.AspNetCore.Builder;
+
+using System.Threading.Tasks;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
-app.MapGet("/", () => "sample");
+
+//! Single Request Handler
+// app.MapGet("/", ()=> "Hello World!");
+
+//! Multiple Request Handler
+
+app.Use(async (context, next) =>
+{
+    app.Logger.LogInformation($"Start Time : {DateTime.Now.ToString()}");
+
+    await next.Invoke();
+
+    app.Logger.LogInformation($"Stop Time : {DateTime.Now.ToString()}");
+});
+
+//* Ordering System
+app.MapGet("/", async () => await Task.FromResult("Hello World!"));
+
+
+
 app.Run();
